83fe06b10a7493857ce987ca28c5ec19bfe1cc26
teo-dimitrov <73136771+teo-dimitrov@users.noreply.github.com>
2022-02-16 01:26:19 +0200

    some html/css corrections at home page and orders-all.html

--- src/main/java/com/example/mybusinessmanager_final_project/model/view/OrderViewModel.java
+++ src/main/java/com/example/mybusinessmanager_final_project/model/view/OrderViewModel.java
@@ -3,12 +3,29 @@ package com.example.mybusinessmanager_final_project.model.view;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class OrderViewModel {
     private Long id;
     private String name;
     private Instant created;
     private LocalDateTime deadLine;
+    private String instantStr;
+
+    private String deadlineStr;
+
+
+    public String getInstantStr() {
+        instantStr = created.truncatedTo(ChronoUnit.SECONDS).toString().replaceAll("[TZ]", " ");
+        return instantStr;
+    }
+
+    public OrderViewModel setInstantStr(String instantStr) {
+        this.instantStr = instantStr;
+        return this;
+    }
+
 
     public Long getId() {
         return id;
@@ -28,6 +45,7 @@ public class OrderViewModel {
         return this;
     }
 
+
     public Instant getCreated() {
         return created;
     }
@@ -45,4 +63,15 @@ public class OrderViewModel {
         this.deadLine = deadLine;
         return this;
     }
+
+//    public String getDeadlineStr() {
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+//        deadlineStr = deadLine.format(formatter);
+//        return deadlineStr;
+//    }
+//
+//    public OrderViewModel setDeadlineStr(String deadlineStr) {
+//        this.deadlineStr = deadlineStr;
+//        return this;
+//    }
 }
